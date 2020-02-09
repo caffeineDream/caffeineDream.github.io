9b8b8498862675e0f3150cf44ce8ba6b6ceea6eb
caffeineDream <sweetsleepomine@gmail.com>
2020-02-09 20:12:12 +0300

    verse

--- script/main.js
+++ script/main.js
@@ -13,3 +13,26 @@ menu.addEventListener('click', () => {
         open = false;
     }
 });
+
+const brusovButton = document.getElementById('brusovPriceButton');
+const povarButton = document.getElementById('povarPriceButton');
+const brusovPriceTable = document.getElementById('brusovPriceTable');
+const povarPriceTable = document.getElementById('povarPriceTable');
+
+
+povarButton.addEventListener('click', () => {
+    brusovButton.classList.remove('button-active');
+    brusovButton.removeAttribute('disabled');
+    brusovPriceTable.classList.add('display-none');
+    povarButton.classList.add('button-active');
+    povarButton.setAttribute("disabled", "disabled");
+    povarPriceTable.classList.remove('display-none');
+});
+brusovButton.addEventListener('click', () => {
+    povarButton.classList.remove('button-active');
+    povarButton.removeAttribute('disabled');
+    povarPriceTable.classList.add('display-none');
+    brusovButton.classList.add('button-active');
+    brusovButton.setAttribute("disabled", "disabled");
+    brusovPriceTable.classList.remove('display-none');
+});
