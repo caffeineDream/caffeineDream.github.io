741e30667c48b11e6be343f53f27142370f87b8f
caffeineDream <sweetsleepomine@gmail.com>
2020-02-17 01:27:59 +0300

    verse

--- script/main.js
+++ script/main.js
@@ -14,10 +14,15 @@ menu.addEventListener('click', () => {
     }
 });
 
-
+// Tab buttons
 var shownContent = 'brusovContent';
+var shownCardBrusov = 'zalCardBrusov';
+var shownCardPovar = 'zalCardPovar';
 var tabButtons = document.getElementsByClassName('tab-button');
-var swapContent = function () {
+var galleryButtonsBrusov = document.getElementsByClassName('gallery-button-brusov');
+var galleryButtonsPovar = document.getElementsByClassName('gallery-button-povar');
+
+var swapContent = function() {
     document.getElementById(shownContent).classList.add('display-none');
     document.getElementById(this.value).classList.remove('display-none');
     document.querySelector('button:disabled').classList.remove('button-active');
@@ -25,8 +30,38 @@ var swapContent = function () {
     this.classList.add('button-active');
     this.setAttribute('disabled', 'disabled');
     shownContent = this.value;
+    if (this.value == 'povarContent') {
+        shownCard = 'zalCardPovar';
+    } else {
+        shownCard = 'zalCardBrusov'
+    };
+
+};
+var swapCardsBrusov = function() {
+    document.getElementById(shownCardBrusov).classList.remove('display-flex');
+    document.getElementById(shownCardBrusov).classList.add('display-none');
+    document.querySelector('.gallery-button-brusov[value=' + CSS.escape(shownCardBrusov) + ']').removeAttribute('disabled');
+    document.querySelector('.gallery-button-brusov[value=' + CSS.escape(this.value) + ']').setAttribute('disabled', 'disabled');
+    document.getElementById(this.value).classList.remove('display-none');
+    document.getElementById(this.value).classList.add('display-flex');
+    shownCardBrusov = this.value;
+};
+var swapCardsPovar = function() {
+    document.getElementById(shownCardPovar).classList.remove('display-flex');
+    document.getElementById(shownCardPovar).classList.add('display-none');
+    document.querySelector('.gallery-button-povar[value=' + CSS.escape(shownCardPovar) + ']').removeAttribute('disabled');
+    document.querySelector('.gallery-button-povar[value=' + CSS.escape(this.value) + ']').setAttribute('disabled', 'disabled');
+    document.getElementById(this.value).classList.remove('display-none');
+    document.getElementById(this.value).classList.add('display-flex');
+    shownCardPovar = this.value;
 };
 
 for (var i = 0; i < tabButtons.length; i++) {
     tabButtons[i].addEventListener('click', swapContent, false);
-}
+};
+for (var i = 0; i < galleryButtonsBrusov.length; i++) {
+    galleryButtonsBrusov[i].addEventListener('click', swapCardsBrusov, false)
+};
+for (var i = 0; i < galleryButtonsPovar.length; i++) {
+    galleryButtonsPovar[i].addEventListener('click', swapCardsPovar, false)
+};
