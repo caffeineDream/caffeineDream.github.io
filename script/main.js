298355c59225cabd5e38667afd3efc231de1b7ab
caffeineDream <sweetsleepomine@gmail.com>
2020-03-08 18:34:19 +0300

    verse

--- script/main.js
+++ script/main.js
@@ -16,9 +16,11 @@ menu.addEventListener('click', () => {
 // Tab and page buttons
 var shownContent = 'brusovContent';
 var shownCardBrusov = 'zalCardBrusov';
+var shownCardBrusov2 = '1CardBrusov2';
 var shownCardPovar = 'zalCardPovar';
 var tabButtons = document.getElementsByClassName('tab-button');
 var galleryButtonsBrusov = document.getElementsByClassName('gallery-button-brusov');
+var galleryButtonsBrusov2 = document.getElementsByClassName('gallery-button-brusov2');
 var galleryButtonsPovar = document.getElementsByClassName('gallery-button-povar');
 var swapContent = function() {
     document.getElementById(shownContent).classList.add('display-none');
@@ -48,7 +50,9 @@ var swapCards = function(galleryMarker, subjectButton) {
         shownCardBrusov = subjectButton.value;
     } else if (subjectButton.classList.contains('gallery-button-povar')) {
         shownCardPovar = subjectButton.value;
-    };
+    } else {
+        shownCardBrusov2 = subjectButton.value;
+    }
 };
 for (var i = 0; i < tabButtons.length; i++) {
     tabButtons[i].addEventListener('click', swapContent, false);
@@ -58,6 +62,11 @@ for (var i = 0; i < galleryButtonsBrusov.length; i++) {
         swapCards(shownCardBrusov, this);
     }, false);
 };
+for (var i = 0; i < galleryButtonsBrusov2.length; i++) {
+    galleryButtonsBrusov2[i].addEventListener('click', function() {
+        swapCards(shownCardBrusov2, this);
+    }, false);
+};
 for (var i = 0; i < galleryButtonsPovar.length; i++) {
     galleryButtonsPovar[i].addEventListener('click', function() {
         swapCards(shownCardPovar, this);
@@ -65,16 +74,23 @@ for (var i = 0; i < galleryButtonsPovar.length; i++) {
 };
 // Arrow buttons for mobile
 var brusovCards = document.querySelectorAll('#brusovContent .gallery-card');
+var brusov2Cards = document.querySelectorAll('#brusov2Content .gallery-card');
 var povarCards = document.querySelectorAll('#povarContent .gallery-card');
 var arrowButtons = document.getElementsByClassName('arrow-button');
 var brusovIndex = 0;
+var brusov2Index = 0;
 var povarIndex = 0;
 function arrowClick(currentCardsArray, index, direction ) {
     var maxIndex;
     var currentGallery = currentCardsArray[index].parentElement.id
-    if (currentGallery  == 'brusovContent') {
-        maxIndex = brusovCards.length-1;
-    } else (maxIndex = povarCards.length-1);
+    if (currentGallery == 'brusovContent') {
+        maxIndex = brusovCards.length - 1;
+    } else if (currentGallery == 'povarContent') {
+        maxIndex = povarCards.length - 1;
+    } else {
+        maxIndex = brusov2Cards.length - 1;
+    };
+
     if (direction == 'right' && index < maxIndex) {
         currentCardsArray[index].classList.remove('display-flex');
         currentCardsArray[index].classList.add('display-none');
@@ -82,7 +98,11 @@ function arrowClick(currentCardsArray, index, direction ) {
         currentCardsArray[index+1].classList.add('display-flex');
         if (currentGallery == 'brusovContent') {
             brusovIndex++;
-        } else { povarIndex++ };
+        } else if (currentGallery == 'povarContent') {
+            povarIndex++;
+        } else {
+            brusov2Index++;
+        };
     } else if (direction == 'left' && index !== 0) {
         currentCardsArray[index].classList.remove('display-flex');
         currentCardsArray[index].classList.add('display-none');
@@ -90,7 +110,11 @@ function arrowClick(currentCardsArray, index, direction ) {
         currentCardsArray[index-1].classList.add('display-flex');
         if (currentGallery == 'brusovContent') {
             brusovIndex--;
-        } else { povarIndex-- };
+        } else if (currentGallery == 'povarContent') {
+            povarIndex--;
+        } else {
+            brusov2Index--;
+        };
     };
 };
 for (var i = 0; i < arrowButtons.length; i++) {
@@ -103,6 +127,20 @@ for (var i = 0; i < arrowButtons.length; i++) {
             arrowClick(povarCards, povarIndex, 'left');
         } else if (this.classList.contains('arrow-povar') && this.classList.contains('arrow-right')) {
             arrowClick(povarCards, povarIndex, 'right');
-        };
+        } else if (this.classList.contains('arrow-brusov2') && this.classList.contains('arrow-left')) {
+            arrowClick(brusov2Cards, brusov2Index, 'left');
+        } else if (this.classList.contains('arrow-brusov2') && this.classList.contains('arrow-right')) {
+            arrowClick(brusov2Cards, brusov2Index, 'right');
+        }
     }, false);
+};
+//YCLIENTS
+if(screen.width > 768){
+    var d = document,
+                h = d.getElementsByTagName('head')[0],
+                s = d.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'https://w163948.yclients.com/widgetJS';
+        h.appendChild(s);
 };
